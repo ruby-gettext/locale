c705ef9cfe44effaa26dc5c78e28714a02b11774
Sutou Kouhei <kou@clear-code.com>
2024-03-18 23:12:00 +0900

    Bump version

--- lib/locale/version.rb
+++ lib/locale/version.rb
@@ -2,12 +2,12 @@
   version - version information of Ruby-Locale
 
   Copyright (C) 2008 Masao Mutoh
-  Copyright (C) 2013-2020  Sutou Kouhei <kou@clear-code.com>
+  Copyright (C) 2013-2024  Sutou Kouhei <kou@clear-code.com>
 
   You may redistribute it and/or modify it under the same
   license terms as Ruby.
 =end
 module Locale
-  VERSION = "2.1.4"
+  VERSION = "2.1.5"
 end
 
